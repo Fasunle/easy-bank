a5972526a8978db0d98cc889e4a127d8ff0ff878
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-25 21:21:03 +0100

    [REFACTOR] Footer component refactored

---
+++ components/Footer.styles.tsx
@@ -0,0 +1,26 @@
+
+
+
+export const btnWithGradientStyle = {
+    textAlign: "center",
+    padding: "1em 3em",
+    transition: "0.3s",
+    borderRadius: "100px",
+    color: "white",
+    fontWeight: "600",
+    opacity: "0.8",
+    bgGradient:"linear(to-br, hsl(136, 65%, 51%) 0, hsl(192, 70%, 51%) 100%)",
+    _hover: {
+        opacity: "1",
+        transform: "scale(1.05)"
+    }
+}
+
+export const mobileFooterContainerStyle = {
+    bg: "primary_1",
+    color: "gray.300", 
+    display: {
+        lg: 'none'
+    },
+    padding: "1rem"
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,22 +10,8 @@ import {
     GridItem
  } from "@chakra-ui/react";
 import {Logo, EasyLink } from "./utils";
+import { btnWithGradientStyle, mobileFooterContainerStyle } from "./Footer.styles";
 
-
-const btnWithGradientStyle = {
-    textAlign: "center",
-    padding: "1em 3em",
-    transition: "0.3s",
-    borderRadius: "100px",
-    color: "white",
-    fontWeight: "600",
-    opacity: "0.8",
-    bgGradient:"linear(to-br, hsl(136, 65%, 51%) 0, hsl(192, 70%, 51%) 100%)",
-    _hover: {
-        opacity: "1",
-        transform: "scale(1.05)"
-    }
-}
 
 const SocialLink = ({href, alt}: {href: string, alt?: string}): JSX.Element => (
     <Link isExternal>
@@ -33,58 +19,92 @@ const SocialLink = ({href, alt}: {href: string, alt?: string}): JSX.Element => (
     </Link>
 );
 
-const Footer = () => (
-   <Grid templateColumns='repeat(15, 1fr)' gap={6} h={'10rem'} sx={{bg: "primary_1", color: "gray.300"}}>
-        <GridItem w='100%' colStart={2} colSpan={3} h='100%' sx={{display: 'flex'}}>
+const Invite = () => (
+    <VStack gap={'1rem'} p={{sm: '1rem 0 2rem', md: "0"}}>
+        <Box
+            as="button"
+            sx={btnWithGradientStyle}
+            >
+            Request Invite
+        </Box>
 
-            <VStack sx={{justifyContent: "space-around"}}>
+        <Text>© Easybank. All Rights Reserved</Text>
+    </VStack>
+);
+
+const SocialLinks = () => (
+    <HStack spacing={'1rem'} >
+        <SocialLink href="/images/icon-facebook.svg" alt="Facebook Icon" />
+        <SocialLink href="/images/icon-youtube.svg" alt="Youtube Icon" />
+        <SocialLink href="/images/icon-twitter.svg" alt="Facebook Icon" />
+        <SocialLink href="/images/icon-pinterest.svg" alt="Facebook Icon" />
+        <SocialLink href="/images/icon-instagram.svg" alt="Instagram Icon" />
+    </HStack>
+);
+
+{/* TODO: Justify the text */}
+const  AppLinks = () => (
+    <VStack sx={{textAlign: "end"}}>
+        <EasyLink href="/#about" label="About US"/>
+        <EasyLink href="/#contact" label="Contact"/>
+        <EasyLink href="/#blog" label="Blog"/>
+    </VStack>
+);
+
+const  SupportLinks = () => (
+    <VStack>
+        <EasyLink href="/#careers" label="Careers"/>
+        <EasyLink href="/#support" label="Support"/>
+        <EasyLink href="/#policy" label="Privacy Policy"/>
+    </VStack>
+);
+
+
+const Footer = () => (
+   <>
+        <VStack as="footer" sx={mobileFooterContainerStyle}>
+            <VStack sx={{justifyContent: "space-around", h: '8rem'}}>
                 <Box bgColor={'white'}>
                     {<Logo /> || <Heading as="h2">EasyBank</Heading>}
                 </Box>
-                    
-                <HStack spacing={'1rem'} >
-                    <SocialLink href="/images/icon-facebook.svg" alt="Facebook Icon" />
-                    <SocialLink href="/images/icon-youtube.svg" alt="Youtube Icon" />
-                    <SocialLink href="/images/icon-twitter.svg" alt="Facebook Icon" />
-                    <SocialLink href="/images/icon-pinterest.svg" alt="Facebook Icon" />
-                    <SocialLink href="/images/icon-instagram.svg" alt="Instagram Icon" />
-                </HStack>
+                <SocialLinks />
             </VStack>
 
-        </GridItem>
+            <AppLinks/>
+            <SupportLinks/>
+            <Invite />
+        </VStack>
 
-        <GridItem w='100%' colStart={5} colSpan={3} h='100%' sx={{display: 'flex', justifyContent: "start", alignItems: "center"}}>
-            {/* TODO: Justify the text */}
-            <VStack sx={{textAlign: "end"}}>
-                <EasyLink href="/#about" label="About US"/>
-                <EasyLink href="/#contact" label="Contact"/>
-                <EasyLink href="/#blog" label="Blog"/>
-            </VStack>
-        </GridItem>
+        <Grid  templateColumns='repeat(15, 1fr)' gap={6} h={'10rem'} sx={{bg: "primary_1", color: "gray.300", display: {
+            sm: 'none',
+            lg: 'grid'
+        }}}>
+            <GridItem w='100%' colStart={2} colSpan={3} h='100%' sx={{display: 'flex'}}>
 
-        <GridItem w='100%' colStart={8} colSpan={3} h='100%' sx={{display: 'flex', justifyContent: "start", alignItems: "center"}}>
-            <VStack>
-                <EasyLink href="/#careers" label="Careers"/>
-                <EasyLink href="/#support" label="Support"/>
-                <EasyLink href="/#policy" label="Privacy Policy"/>
-            </VStack>
-        </GridItem>
+                <VStack sx={{justifyContent: "space-around"}}>
+                    <Box bgColor={'white'}>
+                        {<Logo /> || <Heading as="h2">EasyBank</Heading>}
+                    </Box>
+                        
+                    <SocialLinks/>
+                </VStack>
+
+            </GridItem>
+
+            <GridItem w='100%' colStart={5} colSpan={3} h='100%' sx={{display: 'flex', justifyContent: "start", alignItems: "center"}}>
+                <AppLinks/>
+            </GridItem>
+
+            <GridItem w='100%' colStart={8} colSpan={3} h='100%' sx={{display: 'flex', justifyContent: "start", alignItems: "center"}}>
+                <SupportLinks/>
+            </GridItem>
 
 
-        <GridItem w='100%' colStart={11} colSpan={4} h='100%' sx={{display: 'flex', justifyContent: "center", alignItems: "center"}}>
-
-            <VStack gap={'1rem'}>
-                <Box
-                    as="button"
-                    sx={btnWithGradientStyle}
-                    >
-                    Request Invite
-                </Box>
-
-                <Text>© Easybank. All Rights Reserved</Text>
-            </VStack>
-        </GridItem>
-    </Grid>
+            <GridItem w='100%' colStart={11} colSpan={4} h='100%' sx={{display: 'flex', justifyContent: "center", alignItems: "center"}}>
+                <Invite />
+            </GridItem>
+        </Grid>
+   </>
 );
 
 export default Footer;
