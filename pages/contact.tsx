6b8f7b51d0521b010335518de14fc4968d82fc05
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-29 23:06:51 +0100

    [FEAT] contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,20 @@
+import { 
+    Box
+} from "@chakra-ui/react";
+import type { NextPage } from 'next'
+
+
+
+
+
+
+const Contact: NextPage = () => {
+    return (
+        <Box as="main">
+           contact component
+        </Box>
+    );
+}
+
+
+export default Contact;
