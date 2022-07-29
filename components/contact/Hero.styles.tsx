8f0833caac3fad054a070d1a4994f692b4257a3c
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-30 00:47:31 +0100

    [REFACTOR] Hero Component

---
+++ components/contact/Hero.styles.tsx
@@ -0,0 +1,25 @@
+
+
+
+export const heroDesktopConatainer = {
+    "gridTemplateColumns": "repeat(12, 1fr)",
+    "gap": "1rem",
+    "h": "400px",
+    "display": {
+        "sm": "none",
+        "lg": "grid"
+    }
+}
+
+export const heroMobileText = {
+    "maxW": "60ch",
+    "p": "0.5rem 1rem",
+    'alignItems': 'flex-start'
+}
+
+export const heroMobileConatainer = {
+    'display': {
+        'sm': 'flex',
+        'md': 'none'
+    }
+}
