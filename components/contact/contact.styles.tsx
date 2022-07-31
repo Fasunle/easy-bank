fbe79c0c6aa5e47fcfadab92996d99634972620f
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-31 03:25:52 +0100

    [FEAT] Invite button styles

---
+++ components/contact/contact.styles.tsx
@@ -0,0 +1,16 @@
+
+
+
+export const btnWithGradientStyle = {
+    padding: "1em 3em",
+    transition: "0.3s",
+    borderRadius: "100px",
+    color: "white",
+    fontWeight: "600",
+    opacity: "0.7",
+    bgGradient:"linear(to-br, hsl(136, 65%, 51%) 0, hsl(192, 70%, 51%) 100%)",
+    _hover: {
+        opacity: "0.5",
+        transform: "scale(1.05)"
+    }
+}
