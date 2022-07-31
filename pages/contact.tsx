5ae8340f565786f43c3f21b51c71dd75d12904b6
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-07-31 03:48:54 +0100

    [FEAT] WhyUs contact page section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,14 @@
 import { Box } from '@chakra-ui/react';
 
-import { Hero } from '../components/contact/Hero';
+import { Hero, WhyUs } from '../components/contact';
 
 import type { NextPage } from 'next';
-
 
 const Contact: NextPage = () => {
     return (
         <Box as="main" mt={'1rem'}>
             <Hero />
+            <WhyUs />
         </Box>
     );
 }
