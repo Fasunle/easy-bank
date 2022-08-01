f65ade7b5cc13910e82cd10936d38c38f6ae5b73
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-08-01 08:58:40 +0100

    [FEAT] image link component

--- components/contact/Articles.tsx
+++ components/contact/Articles.tsx
@@ -1,11 +1,12 @@
 import { Box, Grid, GridItem, Heading, HStack, Text, VStack } from '@chakra-ui/react';
-
+import Image from 'next/image';
 
 type ArticleType = {
     id: number;
     author: string;
     title: string;
     description: string;
+    url: string;
 }
 
 const desktopSectionConatainer = {
@@ -24,25 +25,29 @@ const articles: ArticleType[] = [
         id: 1,
         author: "Claire Robinson",
         title: "Receive money in any currency with no fees",
-        description: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single"
+        description: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single",
+        url: "/images/image-currency.jpg"
     },
     {
         id: 2,
         author: "Wilson Hutton",
         title: "Treat yourself without worrying about money",
-        description: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you"
+        description: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you",
+        url: "/images/image-restaurant.jpg"
     },
     {
         id: 3,
         author: "Wilson Hutton",
         title: "Take your Easybank card wherever you go",
-        description: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you"
+        description: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you",
+        url: "/images/image-confetti.jpg"
     },
     {
         id: 4,
         author: "Claire Robinson",
         title: "Our invite-only Beta accounts are now live!",
-        description: "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site"
+        description: "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site",
+        url: "/images/image-plane.jpg"
     },
 
 ];
