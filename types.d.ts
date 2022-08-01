ec12c716bc429dfd40c246beffa6e04ab9c73336
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-08-01 10:17:47 +0100

    [FEAT] type file

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+type ArticleType = {
+    id: number;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+}
+
+type WhyUsType = {
+    id: string | number;
+    title: string;
+    description: string;
+    url: string;
+}
+
+type EasyLinkType = {
+    label: string, 
+    href: string
+}
